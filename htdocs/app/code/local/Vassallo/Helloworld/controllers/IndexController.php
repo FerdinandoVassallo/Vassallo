eb33e7b5b14e37198c93d0c105c840c170a28265
ferdinando <ferdinando.vassallo6@gmail.com>
2017-10-04 19:41:09 +0200

    Comment

---
+++ htdocs/app/code/local/Vassallo/Helloworld/controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Hello World
+ */
+
+/**
+ * Hello World
+ *
+ * Hello World Index Controller
+ * @author Ferdinando Vassallo <ferdinando.vassallo6@gmail.com>
+ * @package Frontend
+ * @version 0.1.0
+ */
+class Vassallo_Helloworld_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo 'hello';
+    }
+}
+
