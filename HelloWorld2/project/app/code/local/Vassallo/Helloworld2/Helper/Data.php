43d32fd8c8a51e4a96ff18ec76ddc9536dfd14f8
ferdinando <ferdinando.vassallo6@gmail.com>
2017-10-10 16:25:49 +0200

    Vassallo News

---
+++ HelloWorld2/project/app/code/local/Vassallo/Helloworld2/Helper/Data.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Vassallo Helper Data
+ */
+
+/**
+ * Vassallo Helper Data
+ *
+ * @autor Ferdinando Vassallo <ferdinando.vassallo6@gmail.com>
+ * @version 0.1.0
+ * @package Cms
+ *
+ */
+
+class Vassallo_Helloworld2_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
